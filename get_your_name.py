28aa106dd173f7a658c7d1c344edd5d3da837fc9
Aleksandr <fo5u1990@gmail.com>
2019-06-23 22:24:23 +0300

    new cod

--- get_your_name.py
+++ get_your_name.py
@@ -1,13 +1,16 @@
 
+
 class Name():
     def __init__(self,user_name):
-        self.user_name = user_name
+        self.user_name = "aleksandr"
 
 
     def znachenie():
-        my_list = []
+
         my_list.append(number_input)
         result = float(sum(my_list))/ len(my_list)
         print result
 r = Name()
 print r.znachenie()
+
+
