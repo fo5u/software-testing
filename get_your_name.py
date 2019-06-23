09cc04db8cb9e6302208cd5c4dc4c0c6dfd7da76
Aleksandr <fo5u1990@gmail.com>
2019-06-23 21:44:48 +0300

    Fatal Error

---
+++ get_your_name.py
@@ -0,0 +1,13 @@
+
+class Name():
+    def __init__(self,user_name):
+        self.user_name = user_name
+
+
+    def znachenie():
+        my_list = []
+        my_list.append(number_input)
+        result = float(sum(my_list))/ len(my_list)
+        print result
+r = Name()
+print r.znachenie()
